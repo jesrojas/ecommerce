7da9772cff9efcf487a2ba5fe23a78914e2d75d0
Jesus Rojas <jesusarvelais1000@hotmail.com>
2020-02-28 10:34:47 -0800

    First commit, design layout initial stage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { HomePage } from './pages/homepage/homepage.jsx';
+
+function App() {
+  return (
+    <div className="">
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
