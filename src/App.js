0a2c38013a3a94611e3e83ac1e3d3601f24adf49
Jesus Rojas <jesusarvelais1000@hotmail.com>
2020-02-29 14:29:07 -0800

    header component in all pages

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './App.css';
 import { HomePage } from './pages/homepage/homepage.jsx';
 import { ShopPage } from './pages/shopPage/shopPage.jsx';
-import { Switch, Route } from 'react-router-dom';
+import { Header } from './components/header/header.jsx'
 
+import { Switch, Route } from 'react-router-dom';
 // const HatPage = () => (
 //   <div>
 //     <h1>Hat Page</h1>
@@ -13,12 +14,13 @@ import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/'>
           <HomePage/>
         </Route>
 
-        <Route exact path='/shop/hats'>
+        <Route exact path='/shop'>
           <ShopPage/>
         </Route>
       </Switch>
