368c29fcef013788afbf3e5beb4728d522de77e8
Jesus Rojas <jesusarvelais1000@hotmail.com>
2020-04-03 10:58:54 -0700

    updating checkout message

--- client/src/pages/checkout/checkout.jsx
+++ client/src/pages/checkout/checkout.jsx
@@ -36,7 +36,7 @@ const CheckoutPage = ({cartItems, cartTotal}) => (
         </div>
         <div className='test-warning'>*Please use the following test credit card for payments*
             <br/>
-            4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+            4242 4242 4242 4242 - Exp: 01/30 - CVV: 123
         </div>   
         <StripeCheckoutButton price={cartTotal} />
     </div>
